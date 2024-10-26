9deaf37238212056b3c79fa63f6cbc9f71a743d4
asa93 <asa93@live.fr>
2024-10-26 10:12:42 -0400

    update default notary

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,7 +2,7 @@ export const EXPLORER_API = 'https://explorer.tlsnotary.org';
 export const MAX_RECV = 16384;
 export const MAX_SENT = 4096;
 
-export const NOTARY_API = 'https://notary.eternis.ai';
+export const NOTARY_API = 'https://leader.cluster.eternis.ai';
 export const NOTARY_PROXY = 'wss://websockify.cluster.eternis.ai:55688';
 
 // export const NOTARY_API_LOCAL = 'http://localhost:7047';
